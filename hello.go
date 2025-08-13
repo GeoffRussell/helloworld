b05f077fcfd619409c2215b088c97297cbcb3534
Geoff Russell <geoffrey.russell@gmail.com>
2025-08-13 20:34:56 +0930

    Push the rest of the code to github

--- hello.go
+++ hello.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+
+	"github.com/GeoffRussell/helloWorld/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for math.Abs(z*z-x) > 0.000000002 {
+		z -= (z*z - x) / (2 * z)
+		fmt.Println(z)
+	}
+	return z
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	s := Sqrt(2)
+	fmt.Println("Sqrt of 2:  ", s, " check s*s: ", s*s)
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println("Hello, world \u00E9 é")
+	fmt.Println(cmp.Diff("Hello, world\nXXX", "Hello, Go\nYYY"))
 }
